d59d804d1292f34ec7336feaec22f0438553ed40
Stevie <pkka.armor@gmail.com>
2021-12-20 22:48:22 +1000

    too busy day at work but finished one task and started another - Perfect power

--- CodeWarsTasks.py
+++ CodeWarsTasks.py
@@ -458,11 +458,41 @@ two different items from that array).
     twoSum [1, 2, 3] 4 === (0, 2)
 
 """
-def two_sum(numbers, target):
-    numbers = sorted(numbers)
-    for idx, first_number in enumerate(reversed(numbers)):
-        if target - first_number in numbers:
-            return(tuple((len(numbers) - idx - 1, numbers.index(target - first_number))))
+# def two_sum(numbers, target):
+#     for idx, first_number in enumerate(reversed(numbers)):
+#         if target - first_number in numbers:
+#             return(tuple((len(numbers) - idx - 1, numbers.index(target - first_number))))
 
 
-print(two_sum([1234,5678,9012], 14690)) 
+# print(two_sum([1234,5678,9012], 14690)) 
+
+
+""" What's a Perfect Power anyway?
+
+    A perfect power is a classification of positive integers:
+    "In mathematics, a perfect power is a positive integer that can be expressed 
+as an integer power of another positive integer. More formally, n is a perfect 
+power if there exist natural numbers m > 1, and k > 1 such that mk = n."
+    Your task is to check wheter a given integer is a perfect power. If it is a 
+perfect power, return a pair m and k with mk = n as a proof. Otherwise return 
+Nothing, Nil, null, NULL, None or your language's equivalent.
+    Note: For a perfect power, there might be several pairs. For example 
+81 = 3^4 = 9^2, so (3,4) and (9,2) are valid solutions. However, the tests take 
+care of this, so if a number is a perfect power, return any pair that proves it.
+
+Examples
+isPP(4) => [2,2]
+isPP(9) => [3,2]
+isPP(5) => None
+
+"""
+import math
+from fractions import Fraction
+
+def isPP(n):
+    for k in range(2, round(math.sqrt(n) + 1)):
+        if (Fraction(n, 1) ** Fraction(1, k)) % 1 == 0:
+            return [n ** (1. / k), k]
+    return None
+
+print(isPP(125))  
